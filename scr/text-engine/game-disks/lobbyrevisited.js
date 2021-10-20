017e806f3e8f55a70074783b7ff52063c682f8bc
natalielusk <77769252+natalielusk@users.noreply.github.com>
2021-10-20 10:54:02 -0700

    add more desc and rooms
    added more descriptions and rooms to lobbyrevisited.js

--- scr/text-engine/game-disks/lobbyrevisited.js
+++ scr/text-engine/game-disks/lobbyrevisited.js
@@ -1,10 +1,10 @@
 const lobbyRevisited = {
-    roomId = 'lobb-revi', //this is the Lobby Revisited Node, entered from the Chapel node
+    roomId: 'lobb-revi', //this is the Lobby Revisited Node, entered from the Chapel node
     rooms: [
         {
             roomId: 'lobb-revi',
             name: 'The Lobby',
-            desc: ``,
+            desc: `You are now in the lobby.`,
             
             onLook: () => {
                 const room = getRoom('lobb-revi');
@@ -19,28 +19,28 @@ const lobbyRevisited = {
 
                 Beyond the staircase, at the end of a mirror-lined corridor, is an entrance to the Rathskeller Bar and Grill, and at the far end of the corridor is the exit to 52nd St.
 
-                Directly in front of the elevator alcove in which you're standing is the main reception area. In the far corner of the reception area a lonely TV mutely displays the evening news. Near the TV area is a large couch and table, which serves as a lounge. 
+                Directly in front of the elevator alcove in which you're standing is the main reception area. In the far corner of the reception area a lonely TV mutely displays the late news to an empty wingback chair. Near the TV area is a large couch and table, which serves as a lounge. 
                 
                 There is also a door between the reception desk and the 53rd St. exit, that leads to a cubicle of safe deposit boxes.`;
-            }, //maybe change this to being the desc by default without player needing to command look? Lobby Revisited doesn't have a room line introduction
+            }, 
             
             items: [
                 {
 
                 }
-            ],//
-        },//end of lobb-revi room
+            ],
+        },// closes lobb-revi room
         {
             id: 'lobb-revi-1',
             name: 'Newsstand',
-            desc: `The upper part of the newsstand us given order to a display of gum and candy. The newspapers are stacked up to knee-height beneath this display. The New York Times has a headline about a terrorist bombing in the Middle East. The Post's headline is ***MOB BOSS IN CASINO SLAYING***. The headline on the Daily News is ***SUBWAY STRIKERS BATTLE POLICE***.`,
+            desc: `The upper part of the newsstand us given order to a display of gum and candy. Beneath this display there are a few remaining copies of the New York Post, and on either side of this are small hand-lettered signs explaining that the Times and Daily News are sold out. The Post's headline is ***MOB BOSS IN CASINO SLAYING***.`,
             
             items: [
                 {
 
                 }
-            ],//
-        },//end of lobb-revi-1 room
+            ],
+        },// closes lobb-revi-1 room
         {
             id: 'lobb-revi-2',
             name: 'Giftshop',
@@ -50,8 +50,8 @@ const lobbyRevisited = {
                 {
 
                 }
-            ],//
-        },//end of lobb-revi-2 room
+            ],
+        },// closes lobb-revi-2 room
         {
             id: 'lobb-revi-3',
             name: 'Second Floor Staircase',
@@ -61,8 +61,8 @@ const lobbyRevisited = {
                 {
 
                 }
-            ],//
-        },//end of lobb-revi-3 room
+            ],
+        },// closes lobb-revi-3 room
         {
             id: 'lobb-revi-4',
             name: 'Rathskeller Bar and Grill',
@@ -72,8 +72,8 @@ const lobbyRevisited = {
                 {
 
                 }
-            ],//
-        },//end of lobb-revi-4 room
+            ],
+        },// closes lobb-revi-4 room
         {
             id: 'lobb-revi-5',
             name: '52nd Street Exit',
@@ -83,8 +83,8 @@ const lobbyRevisited = {
                 {
 
                 }
-            ],//
-        },//end of lobb-revi-5 room
+            ],
+        },// closes lobb-revi-5 room
         {
             id: 'lobb-revi-6',
             name: 'Registration Desk',
@@ -94,8 +94,8 @@ const lobbyRevisited = {
                 {
 
                 }
-            ],//
-        },//end of lobb-revi-6 room
+            ],
+        },// closes lobb-revi-6 room
         {
             id: 'lobb-revi-7',
             name: 'Safe Deposit Cubicle',
@@ -105,8 +105,8 @@ const lobbyRevisited = {
                 {
 
                 }
-            ],//
-        },//end of lobb-revi-7 room
+            ],
+        },// closes lobb-revi-7 room
         {
             id: 'lobb-revi-8',
             name: '53rd Street Exit',
@@ -116,29 +116,59 @@ const lobbyRevisited = {
                 {
 
                 }
-            ],//
-        },//end of lobb-revi-8 room
+            ],
+        },// closes lobb-revi-8 room
         {
             id: 'lobb-revi-9',
-            name: 'Reception Area',
-            desc: ``,
+            name: ['Reception Area', 'Lounge'],
+            desc: `You enter the reception area and find that you can't resist the invitation to relax offered by a large womblike sofa. Some yards away the TV makes a gentle, meaningless murmur, like the water of a brook. You notice that the bellboy who had brought the credit slip to your room is standing half-hidden by the draperies of a window alcove, watching the TV.`,
             
             items: [
                 {
+                    itemId: 'lobbytv',
+                    name: ['TV', 'Television'],
+                    desc: ``,
+                    onLook: () => {
+                        println(`You watch a news program that is in progress-- but you can't be said to listen to it, for a caucus of dissident members of the Noise Abatement League is carrying on a rather noisy argument over the League's platform, and the sound on the TV has been turned quite low.
+                            
+                        You see a smiling reporter with a microphone standing outside a large stone building. Someone entering a limousine refuses to talk with him. Then there is a picture of a burning apartment building being doused by the Fire Department. Then an advertisement for Total (which seems as familiar to you as the face of the Mona Lisa; your amnesia doesn't extend to television ads, apparently), and another advertisement for fur coats.`);
+                        pressEnter('lobb-revi-10');
+                    }
+                }     
+            ],
+            exits: [
+                {
+                    dir: ['e', 'east'],
+                    id: 'lobb-revi-1',
 
+                    // need to print an exit line `You rise from the sofa, making the vinyl cushions sigh, and return to the newsstand in the lobby.`
+                },
+                {
+                    dir: ['w', 'west'],
+                    id: 'lobb-revi-11'
                 }
-            ],//
-        },//end of lobb-revi-9 room
+            ]
+        },// closes lobb-revi-9 room
         {
-            id: 'lobb-revi-10',
-            name: 'Lounge',
-            desc: ``,
+            id: `lobb-revi-10`,
+            name: '',
+            desc: '',
+            onEnter: () => {
+                println(`Momentarily your attention is diverted by the shouts of the contending factions of the caucus of the Noise Abatement League. When you look back at the TV you think you see your own face on the screen. The hair is shorter, and you seem very unhappy. Small wonder, for this fleeting portrait is framed, at the top of the screen, by the word WANTED, and at the bottom by a string of numerals. You strain to hear the announcer's voice and catch only the end of the report:
+                
+                "...killed during his escape from the Texas State Penitentiary at Revoltillo, where the prisoner was serving a two-year sentence for the possession of an illegal substance. He is beleived to be armed and should be considered dangerous." This caution is followed by the weather report. Tomorrow will be another sunny day.`);
+                pressEnter('lobb-revi-10');
+            }
+        }, // closes lobb-revi-10 room (watching TV continuation)
+        {
+            id: `lobb-revi-11`,
+            name: '',
+            desc: `You rise from the sofa, making the vinyl cushions sign, and walk toward the large curtained windows overlooking Fifth Avenue. Because it is dark outside and bright within the hotel, the window glass acts as yet one more mirror in the lobby's maze of mirrors.`,
             
-            items: [
-                {
-
-                }
-            ],//
-        },//end of lobb-revi-10 room
-    ], //end of lobb-revi rooms
-};//end of const lobbyRevisited 
+            exits: {
+                dir: ['e', 'east'],
+                id: 'lobb-revi-9'
+            }
+        }
+    ], // closes lobb-revi rooms
+};// closes const lobbyRevisited 
