192e14488e7bf729ce130f009b0db9fe4cc2d5b9
Ahria <43478751+Starryahri@users.noreply.github.com>
2021-10-29 19:57:49 -0700

    Revert "adding Chelsea "

--- scr/text-engine/game-disks/phoneCalls.js
+++ scr/text-engine/game-disks/phoneCalls.js
@@ -5,11 +5,6 @@ const phoneCalls = {
             id: 'phone-1',
             name: 'Front Desk',
             desc:`You've reached the front desk.`,
-        },
-        {
-            id: 'phone-2',
-            name: 'Room Service',
-            desc: `After five rings a tired voice says, "Room Service. Your room number please?"`,
         }
 
     ], // closes phoneCalls rooms
