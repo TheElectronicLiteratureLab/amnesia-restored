0692f909e050d99411b3844bb83e32d98bbec540
Charlie Klever <klever.charlie@gmail.com>
2021-11-08 19:21:38 -0800

    made progress in restaurants, added a room in the corridor

--- scr/text-engine/game-disks/restaurants.js
+++ scr/text-engine/game-disks/restaurants.js
@@ -1,4 +1,12 @@
 const luncheonette = { // Luncheonette Room
+  /*
+  FOOD ENERGY TOTALS
+  TUNA      28 energy
+  HAM       26 energy
+  TEA       08 energy
+  COFFEE    08 energy
+  BURGER    16 energy
+  */
   roomId: 'lunch-intro', // Set this to the ID of the room you want the player to start in.
   rooms: [
     {
@@ -27,30 +35,92 @@ const luncheonette = { // Luncheonette Room
       },
       onBlock: () => 
       {
-        let orderTotal = 0;
 
-        if(prevInput === ['tuna']){
+        if(prevInput === "tuna"){
           orderTotal + 2.75;
+          energyTotal + 28;
           println('One charlie is ready to go. \n Anything else?');
           
-        }else if(prevInput ===['ham']){
+        }else if(prevInput ==='ham'){
           orderTotal + 2.75;
+          energyTotal + 26;
           println('One kosher special coming right up. \n Anything else?');
           
-        }else if(prevInput ===['tea']){
+        }else if(prevInput ==='tea'){
           orderTotal + 0.45;
+          energyTotal + 8;
           println('One tea is on its way. \n Anything else?');
           
-        }else if(prevInput ===['pepsi']){
+        }else if(prevInput ==='pepsi'){
           orderTotal + 0.85;
+          energyTotal + 8;
           println('One pepsi is on its way. \n Anything else?');
           
-        }else if(prevInput ===['burger']){
+        }else if(prevInput ==='burger'){
           orderTotal + 0.85;
+          energyTotal + 16;
           println('One hamburger is ready to go. \n Anything else?');
           
+        }else if(prevInput ==='leave'){
+          enterRoom('lunch-leave')
+
         }
       },
+      exits: 
+      [
+        {dir: 'no', id: 'lunch-total'},
+        {dir: 'leave', id: 'lunch-leave'},
+      ]
+    },
+    {
+      id: 'lunch-total', // Unique identifier for this room. Entering a room will set the disk's roomId to this.
+      name: 'Luncheonette', // Displayed each time the player enters the room.
+      desc: `The attendant totals up your purchase on the register, and it comes to $${orderTotal}.`,
+      onEnter: () => 
+        {
+          if(orderTotal <= playMon){ // Subtracting
+          playMon - orderTotal;
+          playHung + energyTotal;
+        pressEnter('lunch-payscreen');
+          }else{
+            pressEnter('lunch-nomon'); // if you have no money
+          }
+        },
+    },
+    {
+      id: 'lunch-payscreen', // Unique identifier for this room. Entering a room will set the disk's roomId to this.
+      name: 'Luncheonette', // Displayed each time the player enters the room.
+      desc: `You hand him the money. After paying the bill, you check to see how much money money you have left. You come up with $${playMon}\n
+      \n
+      Your order arrives. You quickly consume your order, then you get up and leave the restaurant.`,
+      onEnter: () => 
+      {
+        orderTotal = 0; // Clearing this variable
+        energyTotal = 0; // Clearing the variable 
+        pressEnter('prevroomid'); // Replace with the last room visited secret formula
+      }
+    },
+    {
+      id: 'lunch-leave', // Unique identifier for this room. Entering a room will set the disk's roomId to this.
+      name: 'Luncheonette', // Displayed each time the player enters the room.
+      desc: `You leave the restaurant`,
+      onEnter: () => 
+        {
+          orderTotal = 0; // Clearing this variable
+          energyTotal = 0; // Clearing the variable 
+          pressEnter(prevroomid); // Replace with the last room visited secret formula
+        },
+    },
+    {
+      id: 'lunch-nomon', // Unique identifier for this room. Entering a room will set the disk's roomId to this.
+      name: 'Luncheonette', // Displayed each time the player enters the room.
+      desc: `Realizing you do not have enough money, you run out of the store and walk hastily down the block to the nearest corner.`,
+      onEnter: () => 
+        {
+          orderTotal = 0; // Clearing this variable
+          energyTotal = 0; // Clearing the variable 
+          pressEnter(prevroomid); // Replace with the last room visited secret formula
+        },
     },
     ],
-}
+  }
--- scr/text-engine/playercharacter.js
+++ scr/text-engine/playercharacter.js
@@ -1,12 +1,14 @@
 let time = new Date('May 4, 1986 09:05:00')
 let playFat = 100;
 let playHung = 100;
-let playMon = 0.00;
+let playMon = 25.00; // Setting to 25 for testing purposes
 let detScore = 0;
 let charScore = 0;
 let survScore = 0;
 let totalScore = (detScore + charScore + survScore);
 let moveCount = 0;
+let orderTotal = 0; // Creating a variable to store the order total
+let energyTotal = 0; // The total amount of energy gained from their order
 let playCoord = [];
 let playCloth = {
     top: [],
